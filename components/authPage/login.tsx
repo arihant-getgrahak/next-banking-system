c7cf79681f40bade3ab66e203e9e8979a84be770
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-16 07:46:36 +0000

    re:change line:47 in components/authPage/login.tsx forgot to pass commit message previous commit -m "Add react-hook-form in register form"

--- components/authPage/login.tsx
+++ components/authPage/login.tsx
@@ -44,7 +44,9 @@ export default function Login() {
         <Button className="w-full">Create account</Button>
         <div>
           <p>
-            Don&apos;'t have an account? {" "}
+            {
+              "Don't have an account?"
+            }
             <Link href="/auth/register" className="underline">
               Register
             </Link>
