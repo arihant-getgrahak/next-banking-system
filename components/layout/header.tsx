8dcda48c322aef6d19e94a9f97280ec3aa4a40bc
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-18 22:46:08 +0530

    Update Project Name in Header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -41,7 +41,7 @@ export const Header = () => {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Arihant Jain
+            Optima Bank
           </div>
         </div>
       </Link>
