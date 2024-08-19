9ce5f06a93d4943128b7a4665f38cd0945fe4c92
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-19 21:25:39 +0530

    add "  experimental: {     missingSuspenseWithCSRBailout: false,   },"   in next.auth.config to handle
      refered by docs

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -1,20 +1,41 @@
+// "use client";
+// import { Suspense } from "react";
+// import { OtpPage } from "@/components/mfa/OtpPage";
+// import { useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// export default function OtpHome() {
+//   const params = useSearchParams();
+//   const [response, setResponse] = useState("");
+
+//   useEffect(() => {
+//     const responseParam = params.get("email");
+//     if (responseParam) {
+//       const decodedResponse = decodeURIComponent(responseParam);
+//       setResponse(decodedResponse);
+//     }
+//   }, [params]);
+
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <OtpPage email={response} />
+//     </Suspense>
+//   );
+// }
+
 "use client";
-import { OtpPage } from "@/components/mfa/OtpPage";
+
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import { OtpPage } from "@/components/mfa/OtpPage";
 
 export default function OtpHome() {
   const params = useSearchParams();
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    const responseParam = params.get("email");
-    if (responseParam) {
-      const decodedResponse = decodeURIComponent(responseParam);
-      setResponse(decodedResponse);
-    }
-  }, [params]);
-
-  
-  return <OtpPage email={response} />;
+  const responseParam = params.get("email");
+  const decodedResponse = decodeURIComponent(responseParam!);
+  return (
+    <Suspense>
+      <OtpPage email={decodedResponse!} />
+    </Suspense>
+  );
 }
